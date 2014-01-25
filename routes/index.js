927edc8c81846e5e63d732b625f9038415548432
Sergey Ovechkin <me@sovechkin.com>
2014-01-25 16:31:59 +0400

    урл установки приложения

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var insales = require('insales');
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/install', function(req, res) {
+  insales.install(req, function(id){
+    (id) ? res.send(200, 'Приложение установлено') : res.send(500, 'Приложение не установлено');
+  })
 });
 
 module.exports = router;
