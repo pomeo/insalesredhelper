71f7d719056ec1c2626abf3620bd148fe2d41b8b
Sergey Ovechkin <me@sovechkin.com>
2014-05-29 16:31:59 +0400

    отключены функции шаблона insales

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var insales = require('insales');
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '' });
 });
 
+// router.get('/js/install', function(req, res) {
+//   var xml = '(function() {'
+//           + 'var fileref = document.createElement(\"script\");'
+//           + 'fileref.setAttribute(\"type\",\"text/javascript\");'
+//           + 'fileref.id = \'rhlpscrtg\';'
+//           + 'fileref.charset=\'utf-8\';'
+//           + 'fileref.async = true;'
+//           + 'fileref.setAttribute(\"src\", \"https://web.redhelper.ru/service/main.js?c=pomeotest1\");'
+//           + 'document.getElementsByTagName(\"head\")[0].appendChild(fileref);'
+//           + '})();';
+// });
+
 router.get('/install', function(req, res) {
-  insales.install(req, function(id){
-    (id) ? res.send(200, 'Приложение установлено') : res.send(500, 'Приложение не установлено');
-  })
+  res.send(200);
 });
 
 router.get('/uninstall', function(req, res) {
-  insales.uninstall(req, function(del){
-    (del === '1') ? res.send(200, 'Приложение удалено') : res.send(500, 'Приложение не удалено');
-  })
+  res.send(200);
 });
 
 module.exports = router;
