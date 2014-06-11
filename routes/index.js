f37c744d658d8869bdb41891e6c1b883c6d22931
Sergey Ovechkin <me@sovechkin.com>
2014-06-11 16:31:59 +0400

    удалён код установки js tag который был для примера

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,6 @@ router.post('/', function(req, res) {
   res.redirect('/');
 });
 
-// router.get('/js/install', function(req, res) {
-//   var xml = '(function() {'
-//           + 'var fileref = document.createElement(\"script\");'
-//           + 'fileref.setAttribute(\"type\",\"text/javascript\");'
-//           + 'fileref.id = \'rhlpscrtg\';'
-//           + 'fileref.charset=\'utf-8\';'
-//           + 'fileref.async = true;'
-//           + 'fileref.setAttribute(\"src\", \"https://web.redhelper.ru/service/main.js?c=pomeotest1\");'
-//           + 'document.getElementsByTagName(\"head\")[0].appendChild(fileref);'
-//           + '})();';
-// });
 
 router.get('/install', function(req, res) {
   res.send(200);
