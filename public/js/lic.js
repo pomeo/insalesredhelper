06fa9556fecbff3c5e5248b8a062a623130b70e7
Sergey Ovechkin <me@sovechkin.com>
2014-07-23 15:52:37 +0400

    js файл для подсчёта тарифа

---
+++ public/js/lic.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("select[name='months'], input[name='operators']").change(function(){
+    var $r = $("input[name='operators']");
+    var $d = $("select[name='months']");
+    if ($r.length == 1 ) {
+      var t = parseInt($r.val(), 10);
+      var n = parseInt($d.val(), 10);
+      if ((t >= 1)&&(t <= 100)) {
+        $(".b-total").html((n*960)*t);
+      }
+    }
+  });
+  $("#licen").ajaxForm(function() {
+    
+  });
+});
