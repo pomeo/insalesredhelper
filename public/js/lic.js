8ac61a46a8c61282816a1fc316d1ed182acaac9b
Sergey Ovechkin <me@sovechkin.com>
2014-07-29 15:49:59 +0400

    таблица с лицензиями

--- public/js/lic.js
+++ public/js/lic.js
@@ -15,4 +15,19 @@ $(document).ready(function() {
       window.location.replace(response);
     }
   });
+  var doCheckLicenses = function () {
+    $.getJSON( "/licenses", function(data) {
+      var items = [];
+      if (data[0]) {
+        items.push("<table class='uk-table uk-table-striped uk-table-hover uk-table-condensed'><thead><tr><th>Операторов</th><th>Длительность</th><th>Цена</th><th>Дата начала</th><th>Дата окончания</th><th>Статус</th></tr></thead><tbody>");
+        data.forEach(function (e) {
+          items.push( "<tr><td>" + e.operators + "</td><td>" + e.months + " мес.</td><td>" + e.amount + " <i class='uk-icon-ruble'></i></td><td>" + e.start + "</td><td>" + e.end + "</td><td>" + e.status + "</td></tr>");
+        });
+        items.push("</tbody></table>");
+        var result = items.join( "" );
+        $("#b-licenses").html(result);
+      }
+    });
+  };
+  setInterval(doCheckLicenses, 2000);
 });
